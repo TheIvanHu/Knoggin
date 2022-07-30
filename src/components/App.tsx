66003a05043e34f49c97c46d41158d9427a1e94b
Emma <xemmasongx@gmail.com>
2022-07-30 14:56:00 -0400

    test html for menu

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,7 +10,7 @@ import { MemoryRouter, Outlet, useLocation } from "react-router";
 
 import Home from "./Home";
 import SudoType from "../pages/SudoType";
-
+import Menu from "./Menu";
 const App = () => {
   const initialSearchParam = new URLSearchParams(window.location.search);
   const initialPath = initialSearchParam.get("path") || "/";
@@ -21,6 +21,7 @@ const App = () => {
         <Route path="" element={<Home />} />
 
         <Route path="/sudotype" element={<SudoType />} />
+        <Route path="/menu" element={<Menu />} />
       </Routes>
     </MemoryRouter>
   );
---
+++ src/components/Menu.tsx
@@ -0,0 +1,23 @@
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+import { Component } from "react";
+
+class Menu extends Component {
+  render() {
+    return (
+      <>
+        <div>
+          <h1>!uso</h1>
+          <button>Play game :3</button>
+        </div>
+        <div>
+          <h3>About this test</h3>
+          <p>hallo hallo</p>>
+        </div>
+      </>
+    );
+  }
+}
+export default Menu;
