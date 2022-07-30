e1786fd71400af595334b197bf21a008e57fb41b
Ada W <adabingw@yahoo.com>
2022-07-30 12:57:41 -0600

    yare yare

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,12 +3,22 @@ import "./../assets/scss/App.scss";
 import Home from "./Home";
 import SudoType from "../pages/SudoType";
 
-const App = () => (
-  <div className="body">
-    <h1>Goodbye World!</h1>
-    <p>Foo to the barz</p>
-    <Home />
-  </div>
-);
+import Home from "./Home";
+import SudoType from "./SudoType";
+
+const App = () => {
+  const initialSearchParam = new URLSearchParams(window.location.search);
+  const initialPath = initialSearchParam.get("path") || "/";
+
+  return (
+    <MemoryRouter initialEntries={["/", initialPath]}>
+      <Routes>
+        <Route path="" element={<Home />} />
+
+        <Route path="/sudotype" element={<SudoType />} />
+      </Routes>
+    </MemoryRouter>
+  );
+};
 
 export default App;
