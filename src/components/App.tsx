3b3f254ada18b5be73293963c384c75ff1f057c5
Ada W <adabingw@yahoo.com>
2022-07-30 12:57:41 -0600

    yare yare

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,29 @@
 import * as React from "react";
 import "./../assets/scss/App.scss";
-import Home from "./Home";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { MemoryRouter, Outlet, useLocation } from "react-router";
 
-const App = () => (
-  <div className="body">
-    <h1>Goodbye World!</h1>
-    <p>Foo to the barz</p>
-    <Home />
-  </div>
-);
+import Home from "./Home";
+import SudoType from "./SudoType";
+
+const App = () => {
+  const initialSearchParam = new URLSearchParams(window.location.search);
+  const initialPath = initialSearchParam.get("path") || "/";
+
+  return (
+    <MemoryRouter initialEntries={["/", initialPath]}>
+      <Routes>
+        <Route path="" element={<Home />} />
+
+        <Route path="/sudotype" element={<SudoType />} />
+      </Routes>
+    </MemoryRouter>
+  );
+};
 
 export default App;
