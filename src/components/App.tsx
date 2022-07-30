384d59dca23ec8b26465379cceb61c194961c909
Ada W <adabingw@yahoo.com>
2022-07-30 12:32:18 -0600

    blingbling commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,29 +1,14 @@
 import * as React from "react";
 import "./../assets/scss/App.scss";
-import {
-  BrowserRouter as Router,
-  NavLink,
-  Routes,
-  Route,
-} from "react-router-dom";
-import { MemoryRouter, Outlet, useLocation } from "react-router";
-
 import Home from "./Home";
 import SudoType from "../pages/SudoType";
 
-const App = () => {
-  const initialSearchParam = new URLSearchParams(window.location.search);
-  const initialPath = initialSearchParam.get("path") || "/";
-
-  return (
-    <MemoryRouter initialEntries={["/", initialPath]}>
-      <Routes>
-        <Route path="" element={<Home />} />
-
-        <Route path="/sudotype" element={<SudoType />} />
-      </Routes>
-    </MemoryRouter>
-  );
-};
+const App = () => (
+  <div className="body">
+    <h1>Goodbye World!</h1>
+    <p>Foo to the barz</p>
+    <Home />
+  </div>
+);
 
 export default App;
