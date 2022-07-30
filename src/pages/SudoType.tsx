72aa9645b73c4da370f1320c9565be7505eed739
Victor Wei <victoryqwei@gmail.com>
2022-07-30 16:03:06 -0400

    feat: add typing mechanism

--- src/pages/SudoType.tsx
+++ src/pages/SudoType.tsx
@@ -1,11 +1,54 @@
 import * as React from "react";
 import "./styles.scss";
 
+const textToMatch = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
 const SudoType: React.FC = () => {
+  const [correctText, setCorrectText] = React.useState("");
+  const [amountOfWrongLetters, setAmountOfWrongLetters] = React.useState(0);
+  const [wpm, setWpm] = React.useState(0);
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Backspace") {
+      setCorrectText(correctText.slice(0, -1));
+    } else {
+      const newCorrectText = correctText + e.key;
+      // Check if the new correct text is the same as the text to match
+      if (textToMatch.indexOf(newCorrectText) === 0) {
+        setCorrectText(newCorrectText);
+      } else {
+        setAmountOfWrongLetters(amountOfWrongLetters + 1);
+      }
+    }
+  };
+
+  const onKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    e.target.value = "";
+  };
+
   return (
     <div id="home">
-      <p>Type Racer</p>
-      {/* <Link to="/race">Start a race</Link> */}
+      <p>SudoType</p>
+
+      <div>
+        <span>{correctText}</span>
+        <span style={{ color: "grey" }}>
+          {textToMatch.slice(correctText.length)}
+        </span>
+      </div>
+
+      <br />
+
+      <input
+        // ref={(ref) => { this.input = ref; }}
+        className="input-race"
+        onKeyDown={onKeyDown}
+        onKeyUp={onKeyUp}
+        // onKeyPress={e => this.onKeyPress(e)}
+        // onChange={value => this.handleChange(value)}
+        // maxLength={quote.length}
+        // disabled={disabled || finished}
+      />
     </div>
   );
 };
