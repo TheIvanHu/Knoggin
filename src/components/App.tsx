2d274d64cbba8fe9a6e0460a07c9636186e58b35
Ada W <adabingw@yahoo.com>
2022-07-30 11:45:45 -0600

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,11 @@
 import * as React from "react";
 import "./../assets/scss/App.scss";
 
-const reactLogo = require("./../assets/img/react_logo.svg");
-
 const App = () => (
   <div className="app">
     <h1>Hello World!</h1>
     <p>Foo to the barz</p>
     <p> lol</p>
-    <img src={reactLogo.default} height="480" />
   </div>
 );
 
