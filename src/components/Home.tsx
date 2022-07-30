043b9813f5fa1e838b331f19f6a334f6bbc253dc
Ada W <adabingw@yahoo.com>
2022-07-30 12:48:50 -0600

    blingbling commits

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -13,6 +13,8 @@ const Home = () => (
       <Row>
         <Col xs={6} className="one">
           <h1 className="header">Lorem ipsum waku waku</h1>
+          <h4>Insert how scientific our stuff is</h4>
+          <h4 className="randomGame">start random game</h4>
         </Col>
         <Col xs={2} className="two">
           uso
