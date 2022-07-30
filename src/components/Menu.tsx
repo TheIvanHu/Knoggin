a1dd0d0b77571aed76c1e170e3c73677449df583
Ada W <adabingw@yahoo.com>
2022-07-30 15:28:28 -0600

    ://

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,16 +7,31 @@ import "bootstrap/dist/js/bootstrap.js";
 import "../assets/scss/Menu.css";
 import styled from "styled-components";
 import { Component } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Menu: React.FC<any> = (props) => {
-// function Menu(props) {
+  const navigate = useNavigate();
+  const startClick = (name) => {
+    switch (name) {
+      case "uso":
+        break;
+      case "sudo":
+        navigate("/sudotype");
+        break;
+      case "strroop":
+        break;
+    }
+  };
+
   return (
     <>
       <div className="container">
         <div className="row align-items-center">
           <div className="col" id="left">
             <h1>{props.title}</h1>
-            <button className="btn">Play game :3</button>
+            <button className="btn" onClick={() => startClick("sudo")}>
+              Play game :3
+            </button>
           </div>
           <div className="col" id="right">
             <h3>About this test</h3>
