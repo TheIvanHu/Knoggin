71bc257ca63a4a8accd5409318523dc47c7f594e
Victor Wei <victoryqwei@gmail.com>
2022-07-30 14:47:08 -0400

    feat: update to memory router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,16 +6,23 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import { MemoryRouter, Outlet, useLocation } from "react-router";
 
 import Home from "./Home";
+import SudoType from "./SudoType";
 
 const App = () => {
+  const initialSearchParam = new URLSearchParams(window.location.search);
+  const initialPath = initialSearchParam.get("path") || "/";
+
   return (
-    <Router>
+    <MemoryRouter initialEntries={["/", initialPath]}>
       <Routes>
         <Route path="" element={<Home />} />
+
+        <Route path="/sudotype" element={<SudoType />} />
       </Routes>
-    </Router>
+    </MemoryRouter>
   );
 };
 
---
+++ src/components/SudoType.tsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class SudoType extends Component {
+  render() {
+    return <div>Bruh</div>;
+  }
+}
+
+export default SudoType;
